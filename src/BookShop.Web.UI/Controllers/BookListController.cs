5322df6edde7af40e0886f93eb8ea2557e813d5a
Yasemen ÇeLik <50753447+yasemenclik@users.noreply.github.com>
2019-07-05 14:47:05 +0300

    Books & BookList Controller completed

---
+++ src/BookShop.Web.UI/Controllers/BookListController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BookShop.Application.BookListServices;
+using BookShop.Application.BookListServices.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Web.UI.Controllers
+{
+    [Authorize]
+    public class BookListController : Controller
+    {
+        private readonly IBookListService _bookListService;
+        public BookListController(IBookListService bookListService)
+        {
+            _bookListService = bookListService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            if (User.IsInRole("Admin"))
+            {
+                return View(await _bookListService.GetAll());
+            }
+
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return View(await _bookListService.GetAllByOwner(userId));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateBookList model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatorUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                var newItem = await _bookListService.Create(model);
+                return RedirectToAction("Index", "BookList");
+            }
+            return View();
+        }
+        public async Task<ActionResult> Delete(int id)
+        {
+            return View(await _bookListService.Get(id));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirm(DeleteBookList model)
+        {
+            if (ModelState.IsValid)
+            {
+                // silme islemini yap 
+                await _bookListService.Delete(model.Id);
+            }
+            // ve
+
+            // liste sayfasina gonder
+            return RedirectToAction("Index", "BookList");
+
+        }
+
+        public async Task<ActionResult> Update(int id)
+        {
+            var model = await _bookListService.Get(id);
+            UpdateBookList updateModel = new UpdateBookList
+            {
+                Id = model.Id,
+                CreatorUserId = model.CreatorUserId,
+                Title = model.Title
+            };
+            return View(updateModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(UpdateBookList model)
+        {
+            if (ModelState.IsValid)
+            {
+                var updatedBookList = await _bookListService.Update(model);
+
+                UpdateBookList updateModel = new UpdateBookList
+                {
+                    Id = updatedBookList.Id,
+                    CreatorUserId = updatedBookList.CreatorUserId,
+                    Title = updatedBookList.Title
+                };
+                return View(updateModel);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ src/BookShop.Web.UI/Controllers/BooksController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Application.BooksServices;
+using BookShop.Application.BooksServices.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Web.UI.Controllers
+{
+    [Authorize]
+    public class BooksController : Controller
+    {
+        private readonly IBooksService _booksService;
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _booksService.GetAll());
+            
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateBook model)
+        {
+            if (ModelState.IsValid)
+            {
+                // model.CreatorUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                var newItem = await _booksService.Create(model);
+                return RedirectToAction("Index", "Books");
+            }
+            return View();
+        }
+        public async Task<ActionResult> Delete(int id)
+        {
+            return View(await _booksService.Get(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirm(DeleteBook model)
+        {
+            if (ModelState.IsValid)
+            {
+                // silme islemini yap 
+                await _booksService.Delete(model.Id);
+            }
+            // ve
+
+            // liste sayfasina gonder
+            return RedirectToAction("Index", "Books");
+
+        }
+
+        public async Task<ActionResult> Update(int id)
+        {
+            var model = await _booksService.Get(id);
+            UpdateBook updateModel = new UpdateBook
+            {
+                Id = model.Id,
+                CreatorUserId = model.CreatorUserId,
+                Description = model.Description,
+                Name = model.Name,
+                Author = model.Author,
+            };
+            return View(updateModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(UpdateBook model)
+        {
+            if (ModelState.IsValid)
+            {
+                var updatedBook = await _booksService.Update(model);
+
+                UpdateBook updateModel = new UpdateBook
+                {
+                    Id = updatedBook.Id,
+                    CreatorUserId = updatedBook.CreatorUserId,
+                    Description = updatedBook.Description,
+                    Name = updatedBook.Name,
+                    Author = updatedBook.Author
+                };
+                return View(updateModel);
+            }
+            return View(model);
+        }
+    }
+}
